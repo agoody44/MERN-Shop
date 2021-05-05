dab1851de6096c721a10c57ea8439986ff931919
“Alyssa <agoody44@gmail.com>
2021-05-05 17:01:30 -0400

    adds product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// gets all products from db
+// get /api/products
+router.get('/', (req, res) => {
+
+})
+
+// get a product by id from db
+// get /api/products/:id
+router.get('/:id', (req, res) => {
+    
+})
+
+module.exports = router;
