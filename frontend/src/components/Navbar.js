8de38191dd5414ca8833fe3b587c4ab1216ae76d
“Alyssa <agoody44@gmail.com>
2021-05-05 22:01:14 -0400

    adds navbar and styling adds routes and links

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import './Navbar.css';
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            {/*/ Logo */} 
+            <div className='navbar_logo'>
+                <h2>Shopping Cart</h2>
+            </div>
+
+            {/*/ Links */}
+            <ul className='navbar_links'>
+                <li>
+                    <Link to='/cart' className='cart_link'>
+                        {/* Icon */}
+                        <i className='fas fa-shopping-cart'></i>
+                        <span>
+                            Cart
+                            <span className='cartlogo_badge'>0</span>
+                        </span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/'>
+                        Shop                        
+                    </Link>
+                </li>
+            </ul>
+
+            {/*/ Hamburger */}
+            <div className='hamburger_menu'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
