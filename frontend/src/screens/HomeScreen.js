58fe9b45e943039848278f2a9d62fbc4dd0f6619
“Alyssa <agoody44@gmail.com>
2021-05-06 21:05:59 -0400

    Styles Homescreen and Products'

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
 import './HomeScreen.css';
+import Product from "../components/Product";
 
 const HomeScreen = () => {
     return (
         <div className='homescreen'>
-            Home Screen
+            <h2 className='homescreen_title'>Latest Products</h2>
+
+            <div className='homescreen_products'>
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+            </div>
         </div>
     )
 }
