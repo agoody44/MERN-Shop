7b4050e41ee7d783dd9d30f090cb421deaf087e4
“Alyssa <agoody44@gmail.com>
2021-05-10 22:53:23 -0400

    removes router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
-  <Provider store={store}
-  >
+  <Provider store={store}>
     <React.StrictMode>
-      <Router>
         <App />
-      </Router>
     </React.StrictMode>,
   </Provider>,
   document.getElementById('root')
