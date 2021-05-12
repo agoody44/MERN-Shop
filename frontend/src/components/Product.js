8a5653ef5029c84c3933ff5447fe427ad3bcc057
“Alyssa <agoody44@gmail.com>
2021-05-11 21:06:05 -0400

    pulls products from backend

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,20 @@
 import './Product.css';
 import { Link } from 'react-router-dom';
 
-const Product = () => {
+const Product = ({imageUrl, name, price, description, productId}) => {
     return (
         <div className='product'>
-            <img src="https://images.unsplash.com/photo-1616070698578-e5e634af7e8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="product name"/>
+            <img src={imageUrl} alt={name}/>
 
             <div className='product_info'>
-                <p className='info_name'>Product 1</p>
-                <p className='info_description'>                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum odit mollitia natus beatae optio, temporibus quaerat obcaecati blanditiis doloribus cupiditate quidem, assumenda veritatis praesentium vel magni ratione. At, odio reiciendis.</p>
+                <p className='info_name'>{name}</p>
+                <p className='info_description'>                
+                {description.substring(0.100)}...</p>
 
-                <p className='info_price'>$299.99</p>
+                <p className='info_price'>{price}</p>
 
 
-                <Link to={`/product/${111}`} className='info_button'>View</Link>
+                <Link to={`/product/${productId}`} className='info_button'>View</Link>
             </div>
         </div>
     )
