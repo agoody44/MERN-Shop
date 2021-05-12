9a755ee2dcb8f9114ff6496a77dad796464f05b8
“Alyssa <agoody44@gmail.com>
2021-05-12 15:30:23 -0400

    Adds item info to cart screen

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -1,24 +1,23 @@
 import './CartItem.css'
 import { Link } from 'react-router-dom';
 
-const CartItem = () => {
+const CartItem = ({item}) => {
     return (
         <div className='cartitem'>
             <div className='cartitem_image'>
-            <img src="https://images.unsplash.com/photo-1616070698578-e5e634af7e8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="product name"/>
+            <img src={item.imageUrl} alt={item.name}/>
             </div>
 
-            <Link tp={`/product/${111}`} className='cartitem_name'>
-            <p>Product 1</p>
+            <Link tp={`/product/${item.product}`} className='cartitem_name'>
+            <p>{item.name}</p>
             </Link>
 
-            <p className='cartitem_price'>299.99</p>
+            <p className='cartitem_price'>${item.price}</p>
 
-            <select className='cartitem_select'>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
+            <select className='cartitem_select' value={item.qty} onChange={() => console.log('test')}>
+                {[...Array(item.countInStock).keys()].map( x => (
+                    <option key={x+1} value={x+1}>{x+1}</option>
+                ))}
             </select>
 
             <button className='cartitem_deleteBtn'>
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,17 +1,29 @@
 import './CartScreen.css';
 import CartItem from '../components/CartItem'
+// import { useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import{ Link } from 'react-router-dom';
 
 
 const CartScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const cart = useSelector( state => state.cart );
+    const { cartItems } = cart;
+
     return (
         <div className='cartscreen'>
             <div className='cartscreen_left'>
             <h2>Shopping Cart</h2>
-
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
+                {cartItems.length === 0 ? (
+                    <div>
+                        Your cart is empty <Link to='/'>Go Back</Link>
+                    </div>
+                ) : cartItems.map(item => (
+                    <CartItem item={item}/>
+                ))}
             </div>
 
             <div className='cartscreen_right'>
