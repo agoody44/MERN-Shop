94c19197d5c414bf0615bc91e574ca9726013ae6
“Alyssa <agoody44@gmail.com>
2021-05-12 14:48:36 -0400

    fixes line 21 to .

--- backend/controller/productControllers.js
+++ backend/controller/productControllers.js
@@ -15,7 +15,7 @@ const getProductsById = async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
 
-        res,json(product)
+        res.json(product)
     } catch (error) {
         console.log('error')
         res.status(500).json({message: "Server Error"})
