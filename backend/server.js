38e7bce3ff861f6c4aa85edef344e930e590ccd3
“Alyssa <agoody44@gmail.com>
2021-05-12 21:50:17 -0400

    deployment WIP

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,19 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 
-const PORT = process.env.PORT || 5000;
-
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    app.get( '*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
     })
 } else {
     app.get('/', (req,res) => {
         res.send('Shop Running');
     })
 }
+
+const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
